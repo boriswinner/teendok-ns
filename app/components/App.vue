9c573af6ef73f03ddb1345e769999dee199d0a76
Boris Timofeenko <boriswinner88@gmail.com>
2019-05-17 00:53:10 +1000

    Populating

--- app/components/App.vue
+++ app/components/App.vue
@@ -2,8 +2,12 @@
     <Page>
         <ActionBar title="teenDok"/>
         <StackLayout backgroundColor="#3c495e">
-          <RadCalendar class="home__calendar"
+          <RadCalendar class="home__calendar" id="calendar"
               @dateSelected="onDateSelected"
+              :eventSource="calendarEvents"
+              eventsViewMode="Inline" 
+              selectionMode="Single" 
+              viewMode="Month"              
           ></RadCalendar>      
           <Label text="engines" height="70" backgroundColor="#43b883"/>
           <Label text="second" height="70" backgroundColor="#289062"/>
@@ -13,25 +17,56 @@
 </template>
 
 <script >
+  import * as calendarModule from 'nativescript-ui-calendar';
+
   export default {
     data() {
       return {
         msg: 'Hello World!'
       }
     },
+    computed: {
+      calendarEvents (){
+        return this.$store.state.notes
+      }
+    },
+    data () {
+      return {
+        calEv: [],
+      }
+    },
     methods: {
       onDateSelected(args) {
-        console.log("onDateSelected: " + args.date);
-        this.pushNote(args.date, "aaa")
+        // console.log("onDateSelected: " + args.date);
+        this.pushNote(args.date, "ccc")
       },         
       pushNote(tdate, tnote) {
         let t = {
-          date: tdate,
+          startDate: tdate,
+          endDate: tdate,
           note: tnote
         }
         this.$store.commit('addNote', t)
-        console.log(this.$store.state.notes)
+        // console.log(this.$store.state.notes)
       }
+    },
+    created() {
+    let events = [];
+    let now = new Date();
+    let startDate;
+    let endDate;
+    let event;
+    for (let i = 1; i < 10; i++) {
+      startDate = new Date(now.getFullYear(), now.getMonth(), i * 2, 1);
+      endDate = new Date(now.getFullYear(), now.getMonth(), (i * 2), 3);
+      event = new calendarModule.CalendarEvent("event " + i, startDate, endDate, false);
+      events.push(event);
+      if (i % 3 == 0) {
+        event = new calendarModule.CalendarEvent("second " + i, startDate, endDate, true);
+        events.push(event);
+      }
+    }
+    this.$store.state.notes = events;
     }
   }
 </script>
--- app/store.js
+++ app/store.js
@@ -1,19 +1,24 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import * as calendarModule from 'nativescript-ui-calendar';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    notes: {}
+    notes: []
   },
   mutations: {
     addNote (state, payload) {
-      if (Array.isArray(state.notes[payload.date])) {
-        state.notes[payload.date].push(payload.note)
-      } else {
-        state.notes[payload.date] = [payload.note]
-      }
+      console.log('commit')
+      let now = new Date();
+      let startDate;
+      let endDate;      
+      startDate = new Date(now.getFullYear(), now.getMonth(), 1 * 2, 1);
+      endDate = new Date(now.getFullYear(), now.getMonth(), (1 * 2), 3);      
+      let event = new calendarModule.CalendarEvent("as", startDate, endDate)
+      console.log(event)
+      state.notes.push(event)
     }
   },
   actions: {
